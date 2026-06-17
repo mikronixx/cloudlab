38333b994f46099ec7c587cfd5c52ebc42f7ad76
mkrohn-repo <mikron@frieswiththat.org>
2026-06-16 17:24:09 -0700

    reformat to pass check

--- aws/vpc/main.tf
+++ aws/vpc/main.tf
@@ -13,6 +13,6 @@ module "vpc" {
   private_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
   public_subnets  = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
 
-  enable_nat_gateway = false   # skip it, costs money
+  enable_nat_gateway   = false # skip it, costs money
   enable_dns_hostnames = true
 }
